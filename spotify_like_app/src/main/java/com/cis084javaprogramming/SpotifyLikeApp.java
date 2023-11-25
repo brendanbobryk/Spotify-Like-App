f04c9cdc8cb3ecea765fb47fbf5c537de1b6d55f
Brendan Bobryk <babobryk@gmail.com>
2023-11-24 21:39:25 -0800

    Added favorite songs functionality

--- spotify_like_app/src/main/java/com/cis084javaprogramming/SpotifyLikeApp.java
+++ spotify_like_app/src/main/java/com/cis084javaprogramming/SpotifyLikeApp.java
@@ -7,13 +7,12 @@ import java.util.*;
 import javax.sound.sampled.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
-import java.util.ArrayList;
 
 /**
  * Name: Brendan Bobryk
  * Student ID #: 1036738
  * Class: CIS 084 Java Programming
- * Date: 10/30/23
+ * Date: 11/24/23
  * Program: Spotify-Like-App
  */
 
@@ -23,6 +22,8 @@ public class SpotifyLikeApp {
   static private Integer currentSongIndex = -1;
   static private ArrayList<Integer> recentlyPlayedSongIndexes = new ArrayList<Integer>();
   static Clip audioClip;
+  static Integer favoritesCount;
+  static ArrayList<Integer> favouriteArrayList = new ArrayList<Integer>();;
 
   private static String basePath = "C:\\Users\\Brendan\\Documents\\GitHub\\Spotify-Like-App\\spotify_like_app\\src\\main\\java\\com\\cis084javaprogramming";
 
@@ -57,6 +58,7 @@ public class SpotifyLikeApp {
     System.out.println("[H]ome");
     System.out.println("[S]earch by title");
     System.out.println("[L]ibrary");
+    System.out.println("[F]avorites");
     System.out.println("[Q]uit");
 
     System.out.print("\nEnter q to Quit:");
@@ -81,7 +83,7 @@ public class SpotifyLikeApp {
         songSearchIndex = searchByTitle(library, inputtedSongName, songSearchIndex);
         // checks if value was reassigned
         if (songSearchIndex != (-1)) {
-          play(library, songSearchIndex);
+          play(library, songSearchIndex, input);
         } else {
           System.out.println("\n Could not find a song with the given search criteria. \n");
         }
@@ -100,7 +102,26 @@ public class SpotifyLikeApp {
           songIndex = (input.nextInt() - 1);
         }
         // plays selected song
-        play(library, songIndex);
+        play(library, songIndex, input);
+        break;
+      // [F]avorites
+      case "f":
+        System.out.println("-->Favorites<--");
+        // displays favorites library
+        favoritesDisplay(library);
+        if (favoritesCount > 0) {
+          // prompts user to select a song to play if they have favorited songs
+          System.out.println("\nWhich song would you like to play? (1-" + favoritesCount + ")");
+          int favoriteSongIndex = (input.nextInt() - 1);
+          // input validation to ensure the value of the selection is an option listed
+          while (favoriteSongIndex < 0 || favoriteSongIndex > (favoritesCount - 1)) {
+            System.out.println("\nThat is not a valid entry, please enter a number between 1-" + favoritesCount + ".");
+            favoriteSongIndex = (input.nextInt() - 1);
+          }
+          favoriteSongIndex = favouriteArrayList.get(favoriteSongIndex);
+          // plays selected song
+          play(library, favoriteSongIndex, input);
+        }
         break;
       // [Q]uit
       case "q":
@@ -167,8 +188,29 @@ public class SpotifyLikeApp {
     }
   }
 
+  // prints the favorites library
+  public static void favoritesDisplay(JSONArray library) {
+    favouriteArrayList.clear();
+    favoritesCount = 0;
+    // loops through JSONArray and prints the favorited songs
+    for (int i = 0; i < library.size(); i++) {
+      JSONObject obj = (JSONObject) library.get(i);
+      if (obj.get("favorite").equals("true")) {
+        favouriteArrayList.add(i);
+        favoritesCount++;
+        System.out.println(
+            (favoritesCount) + ". " + obj.get("name") + " by " + obj.get("artist") + ", " + obj.get("year") + ", "
+                + obj.get("genre"));
+      }
+    }
+    // provides an output to the user if there are no favorited songs
+    if (favoritesCount == 0) {
+      System.out.println("You have no favorited songs. \n");
+    }
+  }
+
   // plays an audio file
-  public static void play(JSONArray library, Integer songIndex) {
+  public static void play(JSONArray library, Integer songIndex, Scanner input) {
     // get the filePath and open the selected audio file
     JSONObject obj = (JSONObject) library.get(songIndex);
     final String filename = (String) obj.get("filename");
@@ -203,7 +245,38 @@ public class SpotifyLikeApp {
     // prints the details of the song along with the location in the file system
     System.out.println("\nNow playing: " + obj.get("name") + " by " + obj.get("artist") + ", " + obj.get("year") + ", "
         + obj.get("genre"));
+    if (obj.get("favorite").equals("true")) {
+      System.out.println("Favorite: Yes");
+    } else {
+      System.out.println("Favorite: No");
+    }
     System.out.println("Found in: " + filePath + "\n");
+
+    // if the song is in the favorites list, program asks user if they would like it
+    // removed
+    if (obj.get("favorite").equals("true")) {
+      System.out.println("Would you like to remove this song to your favourites? (Y/N)");
+      String favoriteAnswer = input.next().toLowerCase();
+      if (favoriteAnswer.equals("y")) {
+        obj.put((String) "favorite", (String) "false");
+        favouriteArrayList.remove(currentSongIndex);
+        System.out.println("\nFavorites updated.\n");
+      } else {
+        System.out.println("\nNo changes made.\n");
+      }
+    } else {
+      // if the song is not in the favorites list, program asks user if they would
+      // like it added
+      System.out.println("Would you like to add this song to your favourites? (Y/N)");
+      String favoriteAnswer = input.next().toLowerCase();
+      if (favoriteAnswer.equals("y")) {
+        obj.put((String) "favorite", (String) "true");
+        favouriteArrayList.add(currentSongIndex);
+        System.out.println("\nFavorites updated.\n");
+      } else {
+        System.out.println("\nNo changes made.\n");
+      }
+    }
   }
 
   // Func: readJSONFile
